e6d0de5de256caec18fa5de6687e0cdf1d28127c
Narola Harsh <51940885+narolaharsh@users.noreply.github.com>
2026-02-10 20:28:10 +0100

    final updates to the run file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #Bash script to generate frames files for ET and analysing them using DeepExtractor
 outdir="trial20hz"
-detnet='ET2L'
+detnet='ETT'
 label="model1024_network_${detnet}"
 
 python generate_frames.py --outdir ${outdir} \
@@ -11,7 +11,7 @@ python generate_frames.py --outdir ${outdir} \
 --n-glitches 3 \
 --minimum-frequency 20 \
 --signal-catalog ./catalog/t1_catalog.json \
---padding 10  --seed 35 --detector-network ${detnet}
+--padding 10  --seed 2 --detector-network ${detnet}
 python reconstruct.py --outdir ${outdir} \
 --label ${label} --data ${outdir}/${label}_frames.npz --delta-t 0.5
 python plot.py --outdir ${outdir} --label ${label} --snr-data ${outdir}/dex_snr.npz
