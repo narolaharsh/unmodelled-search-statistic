57f9c102d6cc6c285d47a3068317ab08d45ce8cb
Narola Harsh <51940885+narolaharsh@users.noreply.github.com>
2026-02-11 09:12:49 +0100

    Update naming scheme

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,13 +5,28 @@ label="model1024_network_${detnet}"
 
 python generate_frames.py --outdir ${outdir} \
 --label ${label} --inject-signals 1 \
---inject-glitches 0 \
+--inject-glitches 1 \
 --frame-duration 60 \
 --n-signals 1 \
 --n-glitches 3 \
 --minimum-frequency 20 \
 --signal-catalog ./catalog/t1_catalog.json \
---padding 10  --seed 2 --detector-network ${detnet}
+--padding 10  --seed 22 --detector-network ${detnet}
 python reconstruct.py --outdir ${outdir} \
---label ${label} --data ${outdir}/${label}_frames.npz --delta-t 0.5
-python plot.py --outdir ${outdir} --label ${label} --snr-data ${outdir}/dex_snr.npz
+--label ${label} --frames ${outdir}/${label}_frames.npz --delta-t 0.5
+python plot.py --outdir ${outdir} --label ${label} --snr-data ${outdir}/${label}_dex_snr.npz
+
+detnet='ET2L'
+label="model1024_network_${detnet}"
+python generate_frames.py --outdir ${outdir} \
+--label ${label} --inject-signals 1 \
+--inject-glitches 1 \
+--frame-duration 60 \
+--n-signals 1 \
+--n-glitches 3 \
+--minimum-frequency 20 \
+--signal-catalog ./catalog/t1_catalog.json \
+--padding 10  --seed 22 --detector-network ${detnet}
+python reconstruct.py --outdir ${outdir} \
+--label ${label} --frames ${outdir}/${label}_frames.npz --delta-t 0.5
+python plot.py --outdir ${outdir} --label ${label} --snr-data ${outdir}/${label}_dex_snr.npz
